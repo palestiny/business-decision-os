8b8cc0ffebd5cf3b79e86cd58b9b0a0f79367f09
palestiny <153906782+palestiny@users.noreply.github.com>
2026-09-24 23:36:31 +0300

    fix: honor database URL environment in Alembic

--- alembic/env.py
+++ alembic/env.py
@@ -1,4 +1,5 @@
 from logging.config import fileConfig
+import os
 
 from alembic import context
 from sqlalchemy import engine_from_config, pool
@@ -14,14 +15,16 @@ target_metadata = Base.metadata
 
 
 def run_migrations_offline() -> None:
-    url = config.get_main_option("sqlalchemy.url")
+    url = os.getenv("SQLALCHEMY_DATABASE_URL", config.get_main_option("sqlalchemy.url"))
     context.configure(url=url, target_metadata=target_metadata, literal_binds=True, dialect_opts={"paramstyle": "named"})
     with context.begin_transaction():
         context.run_migrations()
 
 
 def run_migrations_online() -> None:
-    connectable = engine_from_config(config.get_section(config.config_ini_section, {}), prefix="sqlalchemy.", poolclass=pool.NullPool)
+    configuration = config.get_section(config.config_ini_section, {})
+    configuration["sqlalchemy.url"] = os.getenv("SQLALCHEMY_DATABASE_URL", configuration["sqlalchemy.url"])
+    connectable = engine_from_config(configuration, prefix="sqlalchemy.", poolclass=pool.NullPool)
     with connectable.connect() as connection:
         context.configure(connection=connection, target_metadata=target_metadata)
         with context.begin_transaction():
